922fa520838c79015fd89136df6438360d2452be
Kirstin Poppen <kirstin.poppen@gmail.com>
2023-08-05 13:30:27 -0500

    recent

--- DataCard.cpp
+++ DataCard.cpp
@@ -26,7 +26,8 @@ int main(int argc, char** argv) {
     
     string postfix="";
     string prefix="";
-    string year = "2016";
+    // changed the year
+    string year = "2018";
     string inputFile="";
     string WP="WP";
     po::variables_map vm;
@@ -36,7 +37,10 @@ int main(int argc, char** argv) {
     ("prefix", po::value<string>(&prefix)->default_value(""))
     ("postfix", po::value<string>(&postfix)->default_value(""))
     //      ("control_region", po::value<int>(&control_region)->default_value(0))
-    ("year", po::value<string>(&year)->default_value("2016"))
+
+    //changed the year
+
+    ("year", po::value<string>(&year)->default_value("2018"))
     ("WP", po::value<string>(&WP)->default_value(""))
     ("inputFile", po::value<string>(&inputFile)->default_value(""));
     po::store(po::command_line_parser(argc, argv).options(config).run(), vm);
@@ -61,99 +65,108 @@ int main(int argc, char** argv) {
     // the vector below specifies a bin name and corresponding bin_id.
     
     //    VString chns = { "mt_qcd","et","em","me","tt"};
-    VString chns = { "mt_qcd","mt_signal","mt_ztt","et_qcd","et_signal","et_ztt","em_qcd","em_signal","em_ztt","tt_qcd","tt_signal","tt_ztt"};
+    VString chns = { "4t_highPurity", "4t_lowPurity"};
+        //"mt_qcd","mt_signal","mt_ztt","et_qcd","et_signal","et_ztt","em_qcd","em_signal","em_ztt","tt_qcd","tt_signal","tt_ztt"};
     map<string, string> input_folders = {
-        {"mt_qcd", "."},
-        {"mt_signal", "."},
-        {"mt_ztt", "."},
-        {"et_qcd", "."},
-        {"et_signal", "."},
-        {"et_ztt", "."},
-        {"em_qcd", "."},
-        {"em_signal", "."},
-        {"em_ztt", "."},
-        {"tt_qcd", "."},
-        {"tt_signal", "."},
-        {"tt_ztt", "."},
+        {"4t_highPurity", "."},
+        {"4t_lowPurity", "."},
+        //{"mt_ztt", "."},
+        //{"et_qcd", "."},
+        //{"et_signal", "."},
+        //{"et_ztt", "."},
+        //{"em_qcd", "."},
+        //{"em_signal", "."},
+        //{"em_ztt", "."},
+        //{"tt_qcd", "."},
+        //{"tt_signal", "."},
+        //{"tt_ztt", "."},
     };
     
     map<string, VString> bkg_procs;
-    bkg_procs["mt_qcd"] = {"QCD", "TT","VV","ZTT"};
-    bkg_procs["mt_signal"] = {"QCD", "TT","VV","ZTT"};
-    bkg_procs["mt_ztt"] = {"QCD", "TT","VV","ZTT"};
-    bkg_procs["et_qcd"] = {"QCD", "TT","VV","ZTT"};
-    bkg_procs["et_signal"] = {"QCD", "TT","VV","ZTT"};
-    bkg_procs["et_ztt"] = {"QCD", "TT","VV","ZTT"};
-    bkg_procs["tt_qcd"] = {"QCD", "TT","VV","ZTT"};
-    bkg_procs["tt_signal"] = {"QCD", "TT","VV","ZTT"};
-    bkg_procs["tt_ztt"] = {"QCD", "TT","VV","ZTT"};
-    bkg_procs["em_qcd"] = {"QCD", "TT","VV","ZTT","W"};
-    bkg_procs["em_signal"] = {"QCD", "TT","VV","ZTT","W"};
-    bkg_procs["em_ztt"] = {"QCD", "TT","VV","ZTT","W"};
+
+    bkg_procs["4t_lowPurity"] = {"output_bkg_DY", "output_bkg_T-tchan", "output_bkg_TT", "output_bkg_Tbar-tW", "output_bkg_VV2l2nu", "output_bkg_WJets", "output_bkg_WZ", "output_bkg_ZZ"};
+
+    //bkg_procs["mt_qcd"] = {"QCD", "TT","VV","ZTT"};
+    //bkg_procs["mt_signal"] = {"QCD", "TT","VV","ZTT"};
+    //bkg_procs["mt_ztt"] = {"QCD", "TT","VV","ZTT"};
+    //bkg_procs["et_qcd"] = {"QCD", "TT","VV","ZTT"};
+    //bkg_procs["et_signal"] = {"QCD", "TT","VV","ZTT"};
+    //bkg_procs["et_ztt"] = {"QCD", "TT","VV","ZTT"};
+    //bkg_procs["tt_qcd"] = {"QCD", "TT","VV","ZTT"};
+    //bkg_procs["tt_signal"] = {"QCD", "TT","VV","ZTT"};
+    //bkg_procs["tt_ztt"] = {"QCD", "TT","VV","ZTT"};
+    //bkg_procs["em_qcd"] = {"QCD", "TT","VV","ZTT","W"};
+    //bkg_procs["em_signal"] = {"QCD", "TT","VV","ZTT","W"};
+    //bkg_procs["em_ztt"] = {"QCD", "TT","VV","ZTT","W"};
+    
+    // check this
+    VString sig_procs = {"output_signal_1000", "output_signal_2000", "output_signal_3000"};
+    
+    //VString mt_cat = {"mt_qcd","mt_signal","mt_ztt"};
+    //VString et_cat = {"et_qcd","et_signal","et_ztt"};
+    //VString tt_cat = {"tt_qcd","tt_signal","tt_ztt"};
+    //VString em_cat = {"em_qcd","em_signal","em_ztt"};
     
     
-    VString sig_procs = {"ggH","XH"};
-    
-    VString mt_cat = {"mt_qcd","mt_signal","mt_ztt"};
-    VString et_cat = {"et_qcd","et_signal","et_ztt"};
-    VString tt_cat = {"tt_qcd","tt_signal","tt_ztt"};
-    VString em_cat = {"em_qcd","em_signal","em_ztt"};
+    //VString qcd_cat = {"mt_qcd","et_qcd","tt_qcd","em_qcd"};
+    //VString sig_cat = {"mt_signal","et_signal","tt_signal","em_signal"};
+    //VString ztt_cat = {"mt_ztt","et_ztt","tt_ztt","em_ztt"};
     
     
-    VString qcd_cat = {"mt_qcd","et_qcd","tt_qcd","em_qcd"};
-    VString sig_cat = {"mt_signal","et_signal","tt_signal","em_signal"};
-    VString ztt_cat = {"mt_ztt","et_ztt","tt_ztt","em_ztt"};
-    
-    
-    VString qcd_cat_tau = {"mt_qcd","et_qcd","tt_qcd"};
-    VString sig_cat_tau = {"mt_signal","et_signal","tt_signal"};
-    VString ztt_cat_tau = {"mt_ztt","et_ztt","tt_ztt"};
+    //VString qcd_cat_tau = {"mt_qcd","et_qcd","tt_qcd"};
+    //VString sig_cat_tau = {"mt_signal","et_signal","tt_signal"};
+    //VString ztt_cat_tau = {"mt_ztt","et_ztt","tt_ztt"};
 
 
-    VString qcd_cat_lt = {"mt_qcd","et_qcd","tt_qcd"};
-    VString sig_cat_lt = {"mt_signal","et_signal","tt_signal"};
-    VString ztt_cat_lt = {"mt_ztt","et_ztt","tt_ztt"};
+    //VString qcd_cat_lt = {"mt_qcd","et_qcd","tt_qcd"};
+    //VString sig_cat_lt = {"mt_signal","et_signal","tt_signal"};
+    //VString ztt_cat_lt = {"mt_ztt","et_ztt","tt_ztt"};
 
     
     
     map<string, Categories> cats;
-    
-    cats["mt_qcd_13TeV"] = {
-        {1, "mt_qcd"},
+    cats["4t_lowPurity"] = {
+        {1, "4t_lowPurity"}
     };
-    cats["mt_signal_13TeV"] = {
-        {1, "mt_signal"},
-    };
-    cats["mt_ztt_13TeV"] = {
-        {1, "mt_ztt"},
-    };
-    cats["et_qcd_13TeV"] = {
-        {1, "et_qcd"},
-    };
-    cats["et_signal_13TeV"] = {
-        {1, "et_signal"},
-    };
-    cats["et_ztt_13TeV"] = {
-        {1, "et_ztt"},
-    };
-    cats["tt_qcd_13TeV"] = {
-        {1, "tt_qcd"},
-    };
-    cats["tt_signal_13TeV"] = {
-        {1, "tt_signal"},
-    };
-    cats["tt_ztt_13TeV"] = {
-        {1, "tt_ztt"},
-    };
-    cats["em_qcd_13TeV"] = {
-        {1, "em_qcd"},
-    };
-    cats["em_signal_13TeV"] = {
-        {1, "em_signal"},
-    };
-    cats["em_ztt_13TeV"] = {
-        {1, "em_ztt"},
-    };
+
+
+
+    //cats["mt_qcd_13TeV"] = {
+        //{1, "mt_qcd"},
+    //};
+    //cats["mt_signal_13TeV"] = {
+        //{1, "mt_signal"},
+    //};
+    //cats["mt_ztt_13TeV"] = {
+        //{1, "mt_ztt"},
+    //};
+    //cats["et_qcd_13TeV"] = {
+        //{1, "et_qcd"},
+    //};
+    //cats["et_signal_13TeV"] = {
+        //{1, "et_signal"},
+    //};
+    //cats["et_ztt_13TeV"] = {
+        //{1, "et_ztt"},
+    //};
+    //cats["tt_qcd_13TeV"] = {
+        //{1, "tt_qcd"},
+    //};
+    //cats["tt_signal_13TeV"] = {
+        //{1, "tt_signal"},
+    //};
+    //cats["tt_ztt_13TeV"] = {
+        //{1, "tt_ztt"},
+    //};
+    //cats["em_qcd_13TeV"] = {
+        //{1, "em_qcd"},
+    //};
+    //cats["em_signal_13TeV"] = {
+        //{1, "em_signal"},
+    //};
+    //cats["em_ztt_13TeV"] = {
+        //{1, "em_ztt"},
+    //};
     
     
     
@@ -170,7 +183,7 @@ int main(int argc, char** argv) {
     //    vector<string> masses = ch::MassesFromRange("800-1500:100");
     //    vector<string> masses = ch::MassesFromRange("14-15:1");
     //vector<string> masses = ch::MassesFromRange("14-15:1");
-    vector<string> masses = {"125"};
+    vector<string> masses = {"1000", "2000", "3000"};
     // Or equivalently, specify the mass points explicitly:
     //! [part2]
     
@@ -228,65 +241,72 @@ int main(int argc, char** argv) {
     //}
     
     if (year.find("2018") != string::npos){
-        cb.cp().process(ch::JoinStr({sig_procs, {"W", "TT","VV","ZTT"}})).AddSyst(cb, "lumi_13TeV_"+year, "lnN", SystMap<era>::init({"13TeV"}, 1.015));
-        cb.cp().process(ch::JoinStr({sig_procs, {"W", "TT","VV","ZTT"}})).AddSyst(cb, "lumi_13TeV_correlated", "lnN", SystMap<era>::init({"13TeV"}, 1.02));
-        cb.cp().process(ch::JoinStr({sig_procs, {"W", "TT","VV","ZTT"}})).AddSyst(cb, "lumi_13TeV_1718", "lnN", SystMap<era>::init({"13TeV"}, 1.002));
+        cb.cp().process(ch::JoinStr({sig_procs, {"output_bkg_DY", "output_bkg_T-tchan", "output_bkg_TT", "output_bkg_Tbar-tW", 
+        "output_bkg_VV2l2nu", "output_bkg_WJets", "output_bkg_WZ", "output_bkg_ZZ"}})).AddSyst(cb, "lumi_13TeV_"+year, "lnN", SystMap<era>::init({"13TeV"}, 1.015));
+        cb.cp().process(ch::JoinStr({sig_procs, {"output_bkg_DY", "output_bkg_T-tchan", "output_bkg_TT", "output_bkg_Tbar-tW", 
+        "output_bkg_VV2l2nu", "output_bkg_WJets", "output_bkg_WZ", "output_bkg_ZZ"}})).AddSyst(cb, "lumi_13TeV_correlated", "lnN", SystMap<era>::init({"13TeV"}, 1.02));
+        cb.cp().process(ch::JoinStr({sig_procs, {"output_bkg_DY", "output_bkg_T-tchan", "output_bkg_TT", "output_bkg_Tbar-tW", 
+        "output_bkg_VV2l2nu", "output_bkg_WJets", "output_bkg_WZ", "output_bkg_ZZ"}})).AddSyst(cb, "lumi_13TeV_1718", "lnN", SystMap<era>::init({"13TeV"}, 1.002));
     }
     
     
     //         electron
-    cb.cp().process(ch::JoinStr({sig_procs, {"W","TT","VV","ZTT"}})).channel(ch::JoinStr({em_cat}))
-        .AddSyst(cb, "CMS_eff_e_em"+year, "lnN", SystMap<era>::init({"13TeV"}, 1.02));
-    cb.cp().process(ch::JoinStr({sig_procs, {"W","TT","VV","ZTT"}})).channel(ch::JoinStr({et_cat}))
-        .AddSyst(cb, "CMS_eff_e_et"+year, "lnN", SystMap<era>::init({"13TeV"}, 1.02));
+    //cb.cp().process(ch::JoinStr({sig_procs, {"W","TT","VV","ZTT"}})).channel(ch::JoinStr({em_cat}))
+        //.AddSyst(cb, "CMS_eff_e_em"+year, "lnN", SystMap<era>::init({"13TeV"}, 1.02));
+    //cb.cp().process(ch::JoinStr({sig_procs, {"W","TT","VV","ZTT"}})).channel(ch::JoinStr({et_cat}))
+        //.AddSyst(cb, "CMS_eff_e_et"+year, "lnN", SystMap<era>::init({"13TeV"}, 1.02));
     
     
-    cb.cp().process(ch::JoinStr({sig_procs, {"W","TT","VV","ZTT"}})).channel(ch::JoinStr({em_cat,et_cat}))
-        .AddSyst(cb, "CMS_scale_e"+year, "lnN", SystMap<era>::init({"13TeV"}, 1.02));
+    //cb.cp().process(ch::JoinStr({sig_procs, {"W","TT","VV","ZTT"}})).channel(ch::JoinStr({em_cat,et_cat}))
+        //.AddSyst(cb, "CMS_scale_e"+year, "lnN", SystMap<era>::init({"13TeV"}, 1.02));
     
-    cb.cp().process(ch::JoinStr({sig_procs, {"W","TT","VV","ZTT"}})).channel(ch::JoinStr({em_cat}))
-        .AddSyst(cb, "CMS_trg_e_em"+year, "lnN", SystMap<era>::init({"13TeV"}, 1.02));
-    cb.cp().process(ch::JoinStr({sig_procs, {"W","TT","VV","ZTT"}})).channel(ch::JoinStr({et_cat}))
-        .AddSyst(cb, "CMS_trg_e_et"+year, "lnN", SystMap<era>::init({"13TeV"}, 1.02));
+    //cb.cp().process(ch::JoinStr({sig_procs, {"W","TT","VV","ZTT"}})).channel(ch::JoinStr({em_cat}))
+        //.AddSyst(cb, "CMS_trg_e_em"+year, "lnN", SystMap<era>::init({"13TeV"}, 1.02));
+    //cb.cp().process(ch::JoinStr({sig_procs, {"W","TT","VV","ZTT"}})).channel(ch::JoinStr({et_cat}))
+        //.AddSyst(cb, "CMS_trg_e_et"+year, "lnN", SystMap<era>::init({"13TeV"}, 1.02));
     
     
     //      muon
-    cb.cp().process(ch::JoinStr({sig_procs, {"W", "TT","VV","ZTT"}})).channel(ch::JoinStr({em_cat}))
-        .AddSyst(cb, "CMS_eff_m_em"+year, "lnN", SystMap<era>::init({"13TeV"}, 1.02));
-    cb.cp().process(ch::JoinStr({sig_procs, {"W", "TT","VV","ZTT"}})).channel(ch::JoinStr({mt_cat}))
-        .AddSyst(cb, "CMS_eff_m_mt"+year, "lnN", SystMap<era>::init({"13TeV"}, 1.02));
+    //cb.cp().process(ch::JoinStr({sig_procs, {"W", "TT","VV","ZTT"}})).channel(ch::JoinStr({em_cat}))
+        //.AddSyst(cb, "CMS_eff_m_em"+year, "lnN", SystMap<era>::init({"13TeV"}, 1.02));
+    //cb.cp().process(ch::JoinStr({sig_procs, {"W", "TT","VV","ZTT"}})).channel(ch::JoinStr({mt_cat}))
+        //.AddSyst(cb, "CMS_eff_m_mt"+year, "lnN", SystMap<era>::init({"13TeV"}, 1.02));
     
     
-    cb.cp().process(ch::JoinStr({sig_procs, {"W", "TT","VV","ZTT"}})).channel(ch::JoinStr({em_cat,mt_cat}))
-        .AddSyst(cb, "CMS_scale_m"+year, "lnN", SystMap<era>::init({"13TeV"}, 1.02));
+    //cb.cp().process(ch::JoinStr({sig_procs, {"W", "TT","VV","ZTT"}})).channel(ch::JoinStr({em_cat,mt_cat}))
+        //.AddSyst(cb, "CMS_scale_m"+year, "lnN", SystMap<era>::init({"13TeV"}, 1.02));
     
-    cb.cp().process(ch::JoinStr({sig_procs, {"W", "TT","VV","ZTT"}})).channel(ch::JoinStr({em_cat}))
-        .AddSyst(cb, "CMS_trg_m_em"+year, "lnN", SystMap<era>::init({"13TeV"}, 1.02));
-    cb.cp().process(ch::JoinStr({sig_procs, {"W", "TT","VV","ZTT"}})).channel(ch::JoinStr({mt_cat}))
-        .AddSyst(cb, "CMS_trg_m_mt"+year, "lnN", SystMap<era>::init({"13TeV"}, 1.02));
-    
-    
-    cb.cp().process(ch::JoinStr({sig_procs, {"W", "TT","VV","ZTT"}})).channel(ch::JoinStr({em_cat,mt_cat,et_cat}))
-        .AddSyst(cb, "CMS_scale_b"+year, "lnN", SystMap<era>::init({"13TeV"}, 1.02));
+    //cb.cp().process(ch::JoinStr({sig_procs, {"W", "TT","VV","ZTT"}})).channel(ch::JoinStr({em_cat}))
+        //.AddSyst(cb, "CMS_trg_m_em"+year, "lnN", SystMap<era>::init({"13TeV"}, 1.02));
+    //cb.cp().process(ch::JoinStr({sig_procs, {"W", "TT","VV","ZTT"}})).channel(ch::JoinStr({mt_cat}))
+        //.AddSyst(cb, "CMS_trg_m_mt"+year, "lnN", SystMap<era>::init({"13TeV"}, 1.02));
     
     
-    cb.cp().process(ch::JoinStr({sig_procs, {"W", "TT","VV","ZTT"}}))
-        .AddSyst(cb, "CMS_res_j"+year, "lnN", SystMap<era>::init({"13TeV"}, 1.01));
+    //cb.cp().process(ch::JoinStr({sig_procs, {"W", "TT","VV","ZTT"}})).channel(ch::JoinStr({em_cat,mt_cat,et_cat}))
+        //.AddSyst(cb, "CMS_scale_b"+year, "lnN", SystMap<era>::init({"13TeV"}, 1.02));
     
-    cb.cp().process({"ZTT"})
-        .AddSyst(cb, "CMS_htt_ZTTNorm"+year, "lnN", SystMap<>::init(1.03));
     
-    cb.cp().process({"TT"})
-        .AddSyst(cb, "CMS_htt_TTNorm"+year, "lnN", SystMap<>::init(1.05));
+    //cb.cp().process(ch::JoinStr({sig_procs, {"W", "TT","VV","ZTT"}}))
+        //.AddSyst(cb, "CMS_res_j"+year, "lnN", SystMap<era>::init({"13TeV"}, 1.01));
     
-    cb.cp().process({"VV"})
-        .AddSyst(cb, "CMS_htt_VVNorm"+year, "lnN", SystMap<>::init(1.05));
+    //cb.cp().process({"ZTT"})
+        //.AddSyst(cb, "CMS_htt_ZTTNorm"+year, "lnN", SystMap<>::init(1.03));
     
-    cb.cp().process({"W"})
-        .AddSyst(cb, "CMS_htt_WNorm"+year, "lnN", SystMap<>::init(1.10));
+    //cb.cp().process({"TT"})
+        //.AddSyst(cb, "CMS_htt_TTNorm"+year, "lnN", SystMap<>::init(1.05));
+    
+    //cb.cp().process({"VV"})
+        //.AddSyst(cb, "CMS_htt_VVNorm"+year, "lnN", SystMap<>::init(1.05));
+    
+    //cb.cp().process({"W"})
+        //.AddSyst(cb, "CMS_htt_WNorm"+year, "lnN", SystMap<>::init(1.10));
+
+
+
+
         
    // QCD norm uncertainties decorrelated across years channel and categories
-    cb.cp().process({"QCD"}) .AddSyst(cb, "CMS_htt_QCDNorm_"+year, "lnN", SystMap<>::init(1.20));    
+    // cb.cp().process({"QCD"}) .AddSyst(cb, "CMS_htt_QCDNorm_"+year, "lnN", SystMap<>::init(1.20));    
     
     
     //####################################################################################
@@ -297,16 +317,16 @@ int main(int argc, char** argv) {
     //  Jet ES
     //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     
-    cb.cp().process(ch::JoinStr({sig_procs, {"W", "TT","VV","ZTT"}}))
-        .AddSyst(cb, "CMS_scale_j"+year, "shape", SystMap<>::init(1.00));
+    //cb.cp().process(ch::JoinStr({sig_procs, {"W", "TT","VV","ZTT"}}))
+        //.AddSyst(cb, "CMS_scale_j"+year, "shape", SystMap<>::init(1.00));
     
     
     //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     //  MET ES
     //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     
-    cb.cp().process(ch::JoinStr({sig_procs, {"W", "TT","VV","ZTT"}}))
-        .AddSyst(cb, "CMS_scale_met_unclustered"+year, "shape", SystMap<>::init(1.00));
+    //cb.cp().process(ch::JoinStr({sig_procs, {"W", "TT","VV","ZTT"}}))
+        //.AddSyst(cb, "CMS_scale_met_unclustered"+year, "shape", SystMap<>::init(1.00));
     
     
     //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -314,14 +334,15 @@ int main(int argc, char** argv) {
     //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     
   
-        cb.cp().process(ch::JoinStr({sig_procs, { "TT","VV","ZTT"}})).channel(ch::JoinStr({tt_cat,mt_cat,et_cat}))
-            .AddSyst(cb, "CMS_scale_t_1prong1pizero"+year, "shape", SystMap<>::init(1.00));
+        //cb.cp().process(ch::JoinStr({sig_procs, { "TT","VV","ZTT"}})).channel(ch::JoinStr({tt_cat,mt_cat,et_cat}))
+            //.AddSyst(cb, "CMS_scale_t_1prong1pizero"+year, "shape", SystMap<>::init(1.00));
     
-        cb.cp().process(ch::JoinStr({sig_procs, { "TT","VV","ZTT"}})).channel(ch::JoinStr({tt_cat,mt_cat,et_cat}))
-            .AddSyst(cb, "CMS_scale_t_1prong"+year, "shape", SystMap<>::init(1.00));
+        //cb.cp().process(ch::JoinStr({sig_procs, { "TT","VV","ZTT"}})).channel(ch::JoinStr({tt_cat,mt_cat,et_cat}))
+            //.AddSyst(cb, "CMS_scale_t_1prong"+year, "shape", SystMap<>::init(1.00));
     
-        cb.cp().process(ch::JoinStr({sig_procs, { "TT","VV","ZTT"}})).channel(ch::JoinStr({tt_cat,mt_cat,et_cat}))
-            .AddSyst(cb, "CMS_scale_t_3prong"+year, "shape", SystMap<>::init(1.00));
+        //cb.cp().process(ch::JoinStr({sig_procs, { "TT","VV","ZTT"}})).channel(ch::JoinStr({tt_cat,mt_cat,et_cat}))
+            //.AddSyst(cb, "CMS_scale_t_3prong"+year, "shape", SystMap<>::init(1.00));
+
     
     
    
