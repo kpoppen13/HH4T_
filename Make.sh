7087e216e54117a62c49e205fdf72fa2a8a57432
Kirstin Poppen <kirstin.poppen@gmail.com>
2023-08-10 10:46:48 -0500

    first commit

---
+++ Make.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+if [ ! $1 ] ; then
+echo "Please specify the code you want to compile by typing :"
+echo "./Make <Your-Code.C>"
+exit 1
+fi
+echo "================================================================"
+
+echo "====> Here are the libraries:" "\n" `root-config --cflags --glibs` "\n"
+filename=`echo $1 | awk -F"." '{print $1}'`
+exefilename=${filename}.exe
+rm -f $exefilename
+g++ $1  -o $exefilename `root-config --cflags --glibs ` -lTMVA -lRooFit -lRooFitCore
+#g++ $1  -o $exefilename `root-config --cflags --glibs`  -lRooFit -lRooFitCore
+   
+echo ""
+if [ -e $exefilename ]; then
+echo "====> Created exe file : "
+ls -lrt $exefilename
+echo "====> Done."
+else
+echo "====> Did not create the exe file!"
+fi
+echo "================================================================"
