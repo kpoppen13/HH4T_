7c76d2e99b965757d559b5af0e322d881be96756
Kirstin Poppen <kirstin.poppen@gmail.com>
2024-01-14 11:17:56 -0600

    updated plots

--- trigger_eff.py
+++ trigger_eff.py
@@ -13,7 +13,6 @@ Rad_3000 = TFile('out_3000.root', 'READ')
 
 # TRIGGER 39, 1 TeV
 ## need to divide the two histograms
-# Get the histograms from the file
 denominator = Rad_1000.Get("Denominator39")
 numerator = Rad_1000.Get("Numerator39")
 
@@ -24,17 +23,56 @@ result_histogram.Divide(denominator)
 c = TCanvas("canvas", "Divided Histogram", 800, 600)
 result_histogram.Draw("colz")
 
-result_histogram.SetTitle("Numerator/Denominator")
-#result_histogram.GetXaxis().SetTitle("X-axis label")
-#result_histogram.GetYaxis().SetTitle("Y-axis label")
+result_histogram.SetTitle("1 TeV MHT and pfMET efficiency trigger 39: HLT_PFHT500_PFMET100_PFMHT100_IDTight_v")
+result_histogram.GetXaxis().SetTitle("MHT")
+result_histogram.GetYaxis().SetTitle("pfMET")
 
 c.Draw()
-c.SaveAs("divided_histogram.pdf")
+c.SaveAs("trigger39_1TeV_eff.pdf")
 
 
 # TRIGGER 39, 2 TeV
+## need to divide the two histograms
+denominator = Rad_2000.Get("Denominator39")
+numerator = Rad_2000.Get("Numerator39")
+
+# Divide numerator by denominator
+result_histogram = numerator.Clone()
+result_histogram.Divide(denominator)
+
+c2 = TCanvas("canvas", "Divided Histogram", 800, 600)
+result_histogram.Draw("colz")
+
+result_histogram.SetTitle("2 TeV MHT and pfMET efficiency trigger 39: HLT_PFHT500_PFMET100_PFMHT100_IDTight_v")
+result_histogram.GetXaxis().SetTitle("MHT")
+result_histogram.GetYaxis().SetTitle("pfMET")
+
+
+c2.Draw()
+c2.SaveAs("trigger39_2TeV_eff.pdf")
+
+
 
 # TRIGGER 39, 3 TeV
+## need to divide the two histograms
+denominator = Rad_3000.Get("Denominator39")
+numerator = Rad_3000.Get("Numerator39")
+
+# Divide numerator by denominator
+result_histogram = numerator.Clone()
+result_histogram.Divide(denominator)
+
+c3 = TCanvas("canvas", "Divided Histogram", 800, 600)
+result_histogram.Draw("colz")
+
+result_histogram.SetTitle("3 TeV MHT and pfMET efficiency trigger 39: HLT_PFHT500_PFMET100_PFMHT100_IDTight_v")
+result_histogram.GetXaxis().SetTitle("MHT")
+result_histogram.GetYaxis().SetTitle("pfMET")
+
+c3.Draw()
+c3.SaveAs("trigger39_3TeV_eff.pdf")
+
+
 
 # TRIGGER 40, 1 TeV
 
