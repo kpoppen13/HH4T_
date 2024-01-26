96834a5544be83980ea93fd3ef051138933ce50a
Kirstin Poppen <kirstin.poppen@gmail.com>
2024-01-26 15:46:08 -0600

    latest version, use this

--- process_trees_4t.cc
+++ process_trees_4t.cc
@@ -97,34 +97,71 @@ void HistTool::histoLoop(std::string year , vector<string> files, string dir, st
         float higgs_pT, higgs_m, m_sv, gen_higgs_pT, gen_leadjet_pT;
         bool isGenTauSub_, isGenTauLead_;
 
-        tree->SetBranchAddress("isGenTauLead_",&isGenTauLead_);
-        tree->SetBranchAddress("isGenTauSub_",&isGenTauSub_);
-        tree->SetBranchAddress("lep1Pt",&lep1Pt_);
-        tree->SetBranchAddress("lep2Pt",&lep2Pt_);
-        tree->SetBranchAddress("lep1IsoPassV",&lep1IsoPassV);
-        tree->SetBranchAddress("lep2IsoPassV",&lep2IsoPassV);
-        tree->SetBranchAddress("lep1IsoPassL",&lep1IsoPassL);
-        tree->SetBranchAddress("lep2IsoPassL",&lep2IsoPassL);
-        tree->SetBranchAddress("OS",&OS);
-        tree->SetBranchAddress("SS",&SS);
+        //tree->SetBranchAddress("isGenTauLead_",&isGenTauLead_);
+        //tree->SetBranchAddress("isGenTauSub_",&isGenTauSub_);
+        //tree->SetBranchAddress("lep1Pt",&lep1Pt_);
+        //tree->SetBranchAddress("lep2Pt",&lep2Pt_);
+        //tree->SetBranchAddress("lep1IsoPassV",&lep1IsoPassV);
+        //tree->SetBranchAddress("lep2IsoPassV",&lep2IsoPassV);
+        //tree->SetBranchAddress("lep1IsoPassL",&lep1IsoPassL);
+        //tree->SetBranchAddress("lep2IsoPassL",&lep2IsoPassL);
+        //tree->SetBranchAddress("OS",&OS);
+        //tree->SetBranchAddress("SS",&SS);
+        //tree->SetBranchAddress("tmass",&tmass);
+        //tree->SetBranchAddress("ht",&ht);
+        //tree->SetBranchAddress("st",&st);
+        //tree->SetBranchAddress("Met",&Met);
+        //tree->SetBranchAddress("LeadJetPt",&LeadJetPt);
+        //tree->SetBranchAddress("dR_lep_lep",&dR_lep_lep);
+        //tree->SetBranchAddress("evtwt",&weight);
+        //tree->SetBranchAddress("NN_disc",&NN_disc);
+        //tree->SetBranchAddress("NN_disc_ZTT",&NN_disc_ZTT);
+        //tree->SetBranchAddress("NN_disc_QCD",&NN_disc_QCD);
+        //tree->SetBranchAddress("higgs_pT",&higgs_pT);
+        //tree->SetBranchAddress("higgs_m",&higgs_m);
+        //tree->SetBranchAddress("m_sv",&m_sv);
+        //tree->SetBranchAddress("MuMatchedIsolation",&MuMatchedIsolation);
+        //tree->SetBranchAddress("EleMatchedIsolation",&EleMatchedIsolation);
+        //tree->SetBranchAddress("gen_higgs_pT",&gen_higgs_pT);
+        //tree->SetBranchAddress("gen_leadjet_pT",&gen_leadjet_pT);
+
+
+
         tree->SetBranchAddress("vis_mass",&vis_mass);
-        tree->SetBranchAddress("tmass",&tmass);
-        tree->SetBranchAddress("ht",&ht);
-        tree->SetBranchAddress("st",&st);
-        tree->SetBranchAddress("Met",&Met);
-        tree->SetBranchAddress("LeadJetPt",&LeadJetPt);
-        tree->SetBranchAddress("dR_lep_lep",&dR_lep_lep);
-        tree->SetBranchAddress("evtwt",&weight);
-        tree->SetBranchAddress("NN_disc",&NN_disc);
-        tree->SetBranchAddress("NN_disc_ZTT",&NN_disc_ZTT);
-        tree->SetBranchAddress("NN_disc_QCD",&NN_disc_QCD);
+        tree->SetBranchAddress("vis_mass2",&vis_mass2);
+        tree->SetBranchAddress("LumiWeight",&LumiWeight);
+        tree->SetBranchAddress("pfMET",&pfMET);
         tree->SetBranchAddress("higgs_pT",&higgs_pT);
-        tree->SetBranchAddress("higgs_m",&higgs_m);
-        tree->SetBranchAddress("m_sv",&m_sv);
-        tree->SetBranchAddress("MuMatchedIsolation",&MuMatchedIsolation);
-        tree->SetBranchAddress("EleMatchedIsolation",&EleMatchedIsolation);
-        tree->SetBranchAddress("gen_higgs_pT",&gen_higgs_pT);
-        tree->SetBranchAddress("gen_leadjet_pT",&gen_leadjet_pT);
+        tree->SetBranchAddress("higgs_pT2",&higgs_pT2);
+        tree->SetBranchAddress("rad_eta", &rad_eta);
+        tree->SetBranchAddress("radion_pt", &radion_pt);
+        tree->SetBranchAddress("radion_inv_mass", &radion_inv_mass);
+        tree->SetBranchAddress("HT",&HT);
+        tree->SetBranchAddress("higgs1_dr",&higgs1_dr);
+        tree->SetBranchAddress("higgs2_dr",&higgs2_dr);
+        tree->SetBranchAddress("dphi_H1",&dphi_H1);
+        tree->SetBranchAddress("dphi_H2",&dphi_H2);
+        tree->SetBranchAddress("dr_HH",&dr_HH);
+        tree->SetBranchAddress("dphi_HH",&dphi_HH);
+        tree->SetBranchAddress("TMass_H1",&TMass_H1);
+        tree->SetBranchAddress("TMass_H2",&TMass_H2);
+        tree->SetBranchAddress("TMass_Radion",&TMass_Radion);
+        tree->SetBranchAddress("dphi_H1_MET",&dphi_H1_MET);
+        tree->SetBranchAddress("dphi_H2_MET",&dphi_H2_MET);
+        tree->SetBranchAddress("dphi_rad_MET",&dphi_rad_MET);
+        tree->SetBranchAddress("dphi_H2_Rad",&dphi_H2_Rad);
+        tree->SetBranchAddress("dph_H1_Rad",&dphi_H1_Rad);
+        tree->SetBranchAddress("dr_H2_Rad",&dr_H2_Rad);
+        tree->SetBranchAddress("dr_H1_Rad",&dr_H1_Rad);
+        tree->SetBranchAddress("tau1_h1_pt",&tau1_h1_pt);
+        tree->SetBranchAddress("tau2_h1_pt",&tau2_h1_pt);
+        tree->SetBranchAddress("tau1_h2_pt",&tau1_h2_pt);
+        tree->SetBranchAddress("tau2_h2_pt",&tau2_h2_pt);
+        tree->SetBranchAddress("ratio",&ratio);
+        tree->SetBranchAddress("ratio2",&ratio2);
+        tree->SetBranchAddress("total_efficiency_39",&total_efficiency_39);
+        tree->SetBranchAddress("AK8Mass", &AK8Mass);
+        tree->SetBranchAddress("AK8Pt", &AK8Pt);
         
         // Here we have to call OS/SS method extracter
         std::cout<<" tree->GetEntries() is "<<tree->GetEntries()<<"\n";
@@ -132,25 +169,63 @@ void HistTool::histoLoop(std::string year , vector<string> files, string dir, st
             tree->GetEntry(i);
             
             std::map<std::string, float>  ObsName {
-                {"lep1Pt",lep1Pt_},
-                {"lep2Pt",lep2Pt_},
-                {"lep1IsoPassV",lep1IsoPassV},
-                {"lep2IsoPassV",lep2IsoPassV},
-                {"vis_mass",vis_mass},
-                {"tmass",tmass},
-                {"ht",ht},
-                {"st",st},
-                {"Met",Met},
-                {"LeadJetPt",LeadJetPt},
-                {"dR_lep_lep",dR_lep_lep},
-                {"higgs_pT",higgs_pT},
-                {"higgs_m",higgs_m},
-                {"m_sv",m_sv},
-                {"NN_disc",NN_disc},
-                {"NN_disc_ZTT",NN_disc_ZTT},
-                {"NN_disc_QCD",NN_disc_QCD},
-                {"MuMatchedIsolation",MuMatchedIsolation},
-                {"EleMatchedIsolation",EleMatchedIsolation}
+                //{"lep1Pt",lep1Pt_},
+                //{"lep2Pt",lep2Pt_},
+                //{"lep1IsoPassV",lep1IsoPassV},
+                //{"lep2IsoPassV",lep2IsoPassV},
+                //{"vis_mass",vis_mass},
+                //{"tmass",tmass},
+                //{"ht",ht},
+                //{"st",st},
+                //{"Met",Met},
+                //{"LeadJetPt",LeadJetPt},
+                //{"dR_lep_lep",dR_lep_lep},
+                //{"higgs_pT",higgs_pT},
+                //{"higgs_m",higgs_m},
+                //{"m_sv",m_sv},
+                //{"NN_disc",NN_disc},
+                //{"NN_disc_ZTT",NN_disc_ZTT},
+                //{"NN_disc_QCD",NN_disc_QCD},
+                //{"MuMatchedIsolation",MuMatchedIsolation},
+                //{"EleMatchedIsolation",EleMatchedIsolation}
+
+
+                {"vis_mass", &vis_mass},
+                {"vis_mass2", &vis_mass2},
+                {"LumiWeight", &LumiWeight},
+                {"pfMET", &pfMET},
+                {"higgs_pT", &higgs_pT},
+                {"higgs_pT2", &higgs_pT2},
+                {"rad_eta", &rad_eta},
+                {"radion_pt", &radion_pt},
+                {"radion_inv_mass", &radion_inv_mass},
+                {"HT", &HT},
+                {"higgs1_dr", &higgs1_dr},
+                {"higgs2_dr", &higgs2_dr},
+                {"dphi_H1", &dphi_H1},
+                {"dphi_H2", &dphi_H2},
+                {"dr_HH", &dr_HH},
+                {"dphi_HH", &dphi_HH},
+                {"TMass_H1", &TMass_H1},
+                {"TMass_H2", &TMass_H2},
+                {"TMass_Radion", &TMass_Radion},
+                {"dphi_H1_MET", &dphi_H1_MET},
+                {"dphi_H2_MET", &dphi_H2_MET},
+                {"dphi_rad_MET", &dphi_rad_MET},
+                {"dphi_H2_Rad", &dphi_H2_Rad},
+                {"dph_H1_Rad", &dph_H1_Rad}, //fix this one
+                {"dr_H2_Rad", &dr_H2_Rad},
+                {"dr_H1_Rad", &dr_H1_Rad},
+                {"tau1_h1_pt", &tau1_h1_pt},
+                {"tau2_h1_pt", &tau2_h1_pt},
+                {"tau1_h2_pt", &tau1_h2_pt},
+                {"tau2_h2_pt", &tau2_h2_pt},
+                {"ratio", &ratio},
+                {"ratio2", &ratio2},
+                {"total_efficiency_39", &total_efficiency_39},
+                {"AK8Mass", &AK8Mass},
+                {"AK8Pt", &AK8Pt}
+                
             };
 
 
